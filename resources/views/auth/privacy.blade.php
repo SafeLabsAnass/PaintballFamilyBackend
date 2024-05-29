50f4ec1bc3f5a0adef7c218ee21b1c5e0fa5c30c
Safelaber4 <devops4@safelabs.agency>
2024-05-29 11:07:41 +0100

    Privacy

--- resources/views/auth/privacy.blade.php
+++ resources/views/auth/privacy.blade.php
@@ -97,9 +97,8 @@
 <body>
 <div class="container" >
     <h1>Privacy Policy</h1>
-    <p>Last updated: May 25, 2023</p>
+    <p>Last updated: May 29, 2024</p>
     <p>This Privacy Policy describes Our policies and procedures on the collection, use and disclosure of Your information when You use the Service and tells You about Your privacy rights and how the law protects You.</p>
-    <p>We use Your Personal data to provide and improve the Service. By using the Service, You agree to the collection and use of information in accordance with this Privacy Policy. This Privacy Policy has been created with the help of the <a href="https://www.freeprivacypolicy.com/free-privacy-policy-generator/" target="_blank">Free Privacy Policy Generator</a>.</p>
     <h1>Interpretation and Definitions</h1>
     <h2>Interpretation</h2>
     <p>The words of which the initial letter is capitalized have meanings defined under the following conditions. The following definitions shall have the same meaning regardless of whether they appear in singular or in plural.</p>
@@ -244,6 +243,5 @@
     <p>You are advised to review this Privacy Policy periodically for any changes. Changes to this Privacy Policy are effective when they are posted on this page.</p>
     <h1>Contact Us</h1>
     <p>If you have any questions about this Privacy Policy, You can contact us:</p>
-</div>
-</body>
+</div></body>
 </html>
